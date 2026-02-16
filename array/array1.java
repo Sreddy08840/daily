bf11a30bfe2d76d40fdbbb95013b768c6c742a45
santosh <sreddy08840@gmail.com>
2026-02-16 15:01:54 +0530

    add the arraexample1

---
+++ array/array1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class array1{
+     public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int [] a={1,2,3,4,5};
+        for (int i=0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+         
+
+     }
+}
