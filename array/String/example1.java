cdca807274d12f454951d74524fccaf1d3a28849
santosh <sreddy08840@gmail.com>
2026-02-17 12:28:58 +0530

    String 1 example

---
+++ array/String/example1.java
@@ -0,0 +1,12 @@
+class example1{
+    public static void main(String[] args){
+      String s1= new String("java");
+      String s2="java";
+      if (s1==s2){
+        System.out.println("the refernse are equal");
+    }
+    else{
+        System.out.println("The refernce are unequal");
+    }
+    }
+}
