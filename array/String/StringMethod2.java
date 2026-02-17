c1e23ab6408cbaa6475f4682afa974f6921521f3
santosh <sreddy08840@gmail.com>
2026-02-17 21:51:05 +0530

    String method2 example

---
+++ array/String/StringMethod2.java
@@ -0,0 +1,14 @@
+class StringMethod2 {
+    public static void main(String[] args) {
+    String s1="RajRamMohanRoy";
+    System.out.println(s1.toLowerCase());
+    System.out.println(s1.toUpperCase());
+    System.out.println(s1.length());
+    System.out.println(s1.charAt(5));
+    System.out.println(s1.indexOf('M'));
+    System.out.println(s1.isBlank());
+    System.out.println(s1.isEmpty());
+    System.out.println(s1.toCharArray());
+
+    }
+}
