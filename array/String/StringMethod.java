6eddcbe619b9e713f1353a3956f77fe35bbf6850
santosh <sreddy08840@gmail.com>
2026-02-17 14:18:35 +0530

    String method example

---
+++ array/String/StringMethod.java
@@ -0,0 +1,12 @@
+public class StringMethod {
+    public static void main (String[] args ){
+        String s1 ="SACHIN";
+        String s2 ="SACHIN";
+        String s3=s1.concat(s2);
+        System.out.println(s3);
+        int s4=s1.compareTo(s2);
+        System.out.print(s4);
+
+    }    
+}
+
