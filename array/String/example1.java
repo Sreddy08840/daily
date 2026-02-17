7926de42d641226ae85444c22eb0f262e184fcaf
santosh <sreddy08840@gmail.com>
2026-02-17 12:29:47 +0530

    String 1 example

--- array/String/example1.java
+++ array/String/example1.java
@@ -2,11 +2,12 @@ class example1{
     public static void main(String[] args){
       String s1= new String("java");
       String s2="java";
-      if (s1==s2){
-        System.out.println("the refernse are equal");
+    
+    if (s1.equals(s2)){
+        System.out.println("the content are equal");
     }
     else{
-        System.out.println("The refernce are unequal");
+        System.out.println("The content are unequal");
     }
     }
 }
