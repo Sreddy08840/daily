d6312e9e70cb254c332a140527b5c85779bc72da
santosh <sreddy08840@gmail.com>
2026-02-19 15:21:33 +0530

    enxaplation clear  error example1

--- DSA_JAVA_Week/encaplation/example1.java
+++ DSA_JAVA_Week/encaplation/example1.java
@@ -1,14 +1,28 @@
 class bank{
     private int AccountNumber;
     private int AccountPassword;
+    void setAccountNumber(int x){
+        AccountNumber=x;
+    }
+    void setAccountPassword(int y){
+        AccountPassword=y;
+    }
+    void getAccountNumber(){
+        System.out.println(AccountNumber);
+    }
+    void getAccountPassword(){
+        System.out.println(AccountPassword);
+    }
 
 
 }
 class example1{
     public static void main(String[] args){
       bank b=new bank();
-      System.out.println(b.AccountNumber);
-      System.out.println(b.AccountPassword);
+      b.setAccountNumber(12345);
+      b.setAccountPassword(54321);
+      b.getAccountNumber();
+      b.getAccountPassword();
     }
     
 }
