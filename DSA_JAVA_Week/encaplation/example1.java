0709fb00c114045faa016513410d6a58c388afdd
santosh <sreddy08840@gmail.com>
2026-02-19 15:18:27 +0530

    enxaplation error example1

---
+++ DSA_JAVA_Week/encaplation/example1.java
@@ -0,0 +1,14 @@
+class bank{
+    private int AccountNumber;
+    private int AccountPassword;
+
+
+}
+class example1{
+    public static void main(String[] args){
+      bank b=new bank();
+      System.out.println(b.AccountNumber);
+      System.out.println(b.AccountPassword);
+    }
+    
+}
--- array/String/StringMethod2.java
+++ array/String/StringMethod2.java
@@ -1,8 +1,7 @@
 class StringMethod2 {
     public static void main(String[] args) {
-    String s1="Raj,Ram,Mohan,Roy";
-    /*
-    System.out.println(s1.toLowerCase());
+    String s1=new String("Raj,Ram,Mohan,Roy") ;   /*
+    System.out.println(s1.oLowerCase());
     System.out.println(s1.toUpperCase());
     System.out.println(s1.length());
     System.out.println(s1.charAt(5));
