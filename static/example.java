d49bd9318e8c0b54405a86ce4ff3f5921c347d79
santosh <sreddy08840@gmail.com>
2026-02-21 16:53:53 +0530

    static example

---
+++ static/example.java
@@ -0,0 +1,33 @@
+ class test{
+
+    static int a,b;
+    static{
+        System.out.println("this is a static block");
+    }
+    static void display(){
+        System.out.println("this is a static method");
+        
+    }
+    int x,y;
+    {
+        System.out.println("this is a non static block");
+    }
+    void show(){
+        System.out.println("this is a non static method");
+    }
+
+    public test() {
+        System.out.println("this is a constructor");
+    }
+    
+
+}
+public class example {
+    
+    public static void main(String[] args) {
+          test t1=new test();
+        test.display();
+        
+        t1.show();
+    }
+}
