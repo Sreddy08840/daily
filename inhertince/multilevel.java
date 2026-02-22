ba6973740d8cd8dd309e9302043735b53f5a36a6
santosh <sreddy08840@gmail.com>
2026-02-22 17:30:21 +0530

    inhertince mutilevel

--- inhertince/multilevel.java
+++ inhertince/multilevel.java
@@ -1,1 +1,21 @@
-class mutilevel
+class grandparent{
+	void method(){
+		System.out.println("the multilevel inhertinace ");
+	}
+}
+class parent extends grandparent{
+}
+class child extends parent{
+}
+
+
+
+
+
+
+class multilevel{
+	public static void main(String[] args){
+		child c= new child();
+		c.method();
+	}
+}
