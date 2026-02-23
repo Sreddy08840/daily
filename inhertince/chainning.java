0eb4276bd8d7f909507b1a0358ddfc780737ed12
santosh <sreddy08840@gmail.com>
2026-02-23 15:50:52 +0530

    getting error

---
+++ inhertince/chainning.java
@@ -0,0 +1,35 @@
+class parent{
+	int a ,b;
+	parent(){
+		a=100;
+		b=200;
+	}
+	parent (int a ,int b){
+		this.a=a;
+		this.b=a;
+	}
+}
+class child{
+	int x,y;
+	child(){
+		
+		x=300;
+		y=400;
+	}
+	child(int x,int y){
+		this.x=x;
+		this.y=y;
+	}
+	void display(){
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(x);
+		System.out.println(y);
+}
+}
+class chainning{
+	public static void main(String[] args){
+		child c=new child();
+		c.display();
+	}		
+}
