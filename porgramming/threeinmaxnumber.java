28a4b38c57144c45dacb184c5977e670a8ae74d4
santosh <sreddy08840@gmail.com>
2026-02-23 21:52:08 +0530

    three in max number

---
+++ porgramming/threeinmaxnumber.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class threeinmaxnumber{
+	public static void main(String[] args){
+		Scanner sc =new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		int max=a<b?(b<c?c:b):(a<c?c:a);
+		System.out.println(max);
+	}}
