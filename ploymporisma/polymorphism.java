fc632c12a609d0610443d23871f6ad02e0bfc239
santosh <sreddy08840@gmail.com>
2026-02-25 14:35:05 +0530

    ployprisma

---
+++ ploymporisma/polymorphism.java
@@ -0,0 +1,51 @@
+
+class Plain{
+	void fly() {
+		System.out.println("the plain is flying");
+	}
+	void landing() {
+		System.out.println("the lading is  levelrun way");
+		
+	}
+
+}
+class CargoPlain extends Plain{
+	@Override
+	void fly() {
+		System.out.println("the plain is fly lower range");
+	}
+	@Override
+	void landing() {
+		// TODO Auto-generated method stub
+		System.out.println("the lading is midddile levelrun way");
+	}
+}
+class PassengerPlain extends Plain{
+	@Override
+	void landing() {
+		System.out.println("the lading is midddile levelrun way");
+	}
+		
+	}
+
+	
+
+public class polymorphism {
+	public static void main(String[] args) {
+		Plain ref ;
+		PassengerPlain pp=new PassengerPlain();
+		CargoPlain cp=new CargoPlain();
+		ref=pp;
+		
+		ref.fly();
+		ref.landing();
+		
+		System.out.println();
+		ref=cp;
+		ref.fly();
+		ref.landing();
+		
+		
+	}
+
+}
--- static/example.java
+++ static/example.java
@@ -1,32 +1,21 @@
- class test{
 
-    static int a,b;
-    static{
-        System.out.println("this is a static block");
-    }
-    static void display(){
-        System.out.println("this is a static method");
-        
-    }
-    int x,y;
-    {
-        System.out.println("this is a non static block");
-    }
-    void show(){
-        System.out.println("this is a non static method");
-    }
-
-    public test() {
-        System.out.println("this is a constructor");
-    }
-    
-
+class parent{
+	 final int a=8;
 }
-public class example {
+class child extends parent {
     
-    public static void main(String[] args) {
-        test t1=new test();
-        test.display();
-        t1.show();
-    }
+	final int a =9;
+	void mehod() {
+		System.out.println(a);
+	}
+}
+
+
+
+public class example {
+	public static void main(String[] args) {
+		child c=new child();
+        c.mehod();
+		
+	}
 }
