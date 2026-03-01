33af15378c925e3548eb910d3f310e7becfa7c5f
santosh <sreddy08840@gmail.com>
2026-03-01 20:22:06 +0530

    The examption example 2

---
+++ exaption/Main1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main1 {
+	public static void main(String[] args) {
+		System.out.println("The porgram is satrted");
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter the a c value");
+		int a=scan.nextInt();
+		System.out.println("Enter the b value");
+		int b=scan.nextInt();
+		int c=a/b;
+		System.out.println(c);
+		System.out.println("The porgarm is terminted ");
+	}
+
+}
