b9d6cb29a239aa3c83d92be55c8aa5be68026f11
santosh <sreddy08840@gmail.com>
2026-03-01 20:26:34 +0530

    The examption example 1

--- exaption/Main1.java
+++ exaption/Main1.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
 
-public class Main1 {
+	public class Main1 {
 	public static void main(String[] args) {
 		System.out.println("The porgram is satrted");
+		try {
 		Scanner scan=new Scanner(System.in);
 		System.out.println("Enter the a c value");
 		int a=scan.nextInt();
@@ -10,7 +11,10 @@ public class Main1 {
 		int b=scan.nextInt();
 		int c=a/b;
 		System.out.println(c);
+		}
+		catch(Exception e) {
+			System.out.println("Enterd the correct value:");
+		}
 		System.out.println("The porgarm is terminted ");
 	}
-
 }
