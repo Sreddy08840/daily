b966b72e5888a92560d11bb50ff54c01d8378391
santosh <sreddy08840@gmail.com>
2026-03-04 01:36:49 +0530

    sumOfSallestandSecondSmallest

---
+++ array/sumOfSallestandSecondSmallest.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class sumOfSallestandSecondSmallest  {
+	static int findSecondSmallestsum(int[] a){
+		int smallest=Integer.MAX_VALUE,secondssmall=Integer.MAX_VALUE;
+		for(int i=0;i<a.length;i++) {
+			if(a[i]<smallest) {
+				secondssmall=smallest;
+				smallest=a[i];
+			}
+			else if(a[i]<secondssmall) {
+				secondssmall=a[i];
+			}
+		}
+		return secondssmall+smallest;
+	}
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter the index of the array");
+		int n=scan.nextInt();
+		int[] a=new int[n];
+		for(int i=0;i<a.length;i++) {
+			a[i]=scan.nextInt();
+		}
+		int res=findSecondSmallestsum(a);
+		System.out.println(res);
+	}
+}
