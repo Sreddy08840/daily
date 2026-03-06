d2d9e394b3253f20c65bd1f8b6f1066e01aee356
santosh <sreddy08840@gmail.com>
2026-03-07 01:10:21 +0530

    Pattern Porgarming count

---
+++ pattern porgramming/Demo4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Demo4 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		int count= n*(n+1)/2;
+		
+		System.out.println(count);
+	}
+
+	
+
+}
