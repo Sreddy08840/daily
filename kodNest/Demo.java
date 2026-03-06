313a4636a6b864b896ddebabf578410f94737de0
santosh <sreddy08840@gmail.com>
2026-03-07 01:19:50 +0530

    i alphbate us vowel is not

---
+++ kodNest/Demo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		
+
+
+		
+		char inputChar = 'a';
+		boolean isVowel = (inputChar == 'a'|| inputChar == 'e' || inputChar == 'o' || inputChar == 'i');
+		System.out.println(isVowel);
+
+		
+	}
+
+	
+
+}
