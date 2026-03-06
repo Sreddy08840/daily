e4730cee4f2174f879f8871abbcbd2a40b11c6ed
santosh <sreddy08840@gmail.com>
2026-03-07 01:36:36 +0530

    i alphbate us vowel is not

---
+++ kodNest/Demo1.java
@@ -0,0 +1,20 @@
+public class Demo1 {
+
+	public static void main(String[] args) {
+		
+	boolean var1=true;
+	boolean var2=false;
+	
+	boolean addResult=var1&&var2;
+	System.out.println(var1 +"AND"+var2+"="+addResult);
+	boolean orResult =var1||var2;
+	System.out.println(var1 +"AND"+var2+"="+orResult);
+	boolean notReusult =! var1;
+	System.out.println(notReusult);
+
+		
+	}
+
+	
+
+}
