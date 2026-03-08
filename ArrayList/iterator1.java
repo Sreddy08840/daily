c9eaeca5737fdd66c5b6f06ab1ffe348d4b0b0fc
santosh <sreddy08840@gmail.com>
2026-03-09 00:22:51 +0530

    iterator1 is made to how to use

---
+++ ArrayList/iterator1.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class iterator1 {
+public static void main(String[] args) {
+	ArrayList al1=new ArrayList();
+	al1.add(10);
+	al1.add(20);
+	al1.add(30);
+	al1.add(40);
+//	for(int i=0;i<al1.size();i++) {
+	//	System.out.println(al1.get(i));
+	//}
+	//for (Object e:al1) {
+		//System.out.println(e);
+	//}
+	Iterator itr=al1.iterator();
+	
+	while(itr.hasNext()) {
+	System.out.println(itr.next());
+	}
+}
+}
