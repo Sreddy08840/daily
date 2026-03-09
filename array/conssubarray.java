cc4373cf98855bf9eac7f241bf3f7bc5080531db
santosh <sreddy08840@gmail.com>
2026-03-10 01:54:14 +0530

    consectivesubarray

---
+++ array/conssubarray.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class conssubarray {
+	
+	public static void consectivesubarray(int[] ar1) {
+		for(int i=0;i<ar1.length-1;i++) {
+			if(ar1[i+1]-ar1[i]==1) {
+				System.out.print(ar1[i] +" ");
+			}
+			else {
+				System.out.print(ar1[i]);
+				System.out.println();
+			}
+		}
+		System.out.println(ar1[ar1.length-1]);
+	}
+	 public static void main(String[] args) {
+		 Scanner scan=new Scanner(System.in);
+		 int n=scan.nextInt();
+		 int[] ar1=new int[n];
+		 for(int i=0;i<ar1.length;i++) {
+			 ar1[i]=scan.nextInt();
+		 }
+		consectivesubarray(ar1); 
+	 }
+}
