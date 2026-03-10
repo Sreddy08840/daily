eadc677180ace5663534c1cdc82400e1fc4f71ff
santosh <sreddy08840@gmail.com>
2026-03-11 02:28:53 +0530

    palliendetm

---
+++ String/pallenderam.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class pallenderam {
+	
+	public static String findpallinderm(String s){
+	String t="";
+	int i=0;
+	for ( i = s.length()-1;i >= 0;i--) {
+		t = t+s.charAt(i);
+	}
+	return t;
+}
+	
+	
+	public static void main(String[] args) {
+	Scanner scan=new Scanner(System.in);
+	String s =scan.next();
+	String res =findpallinderm(s);
+	System.out.println(res);
+	if(s.equals(res)) {
+		System.out.println("PAllendirm");
+	}
+	else {
+		System.out.println("Not PallendermS");
+	}
+}
+
+}
