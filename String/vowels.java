1b08c79b8e65917691912d001dc02a15b662201a
santosh <sreddy08840@gmail.com>
2026-03-12 01:33:07 +0530

    find Vowels

---
+++ String/vowels.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class vowels {
+	
+	public static int findvowels(String s){
+		int count =0;
+		for(int i=0;i<s.length();i++) {
+			char ch=s.charAt(i);
+			if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='B')) {
+			if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U') {
+				count ++;
+			}
+			}
+		}
+		return count;
+		
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		String s=scan.nextLine();
+		int res=findvowels(s);
+		System.out.println(res);
+	}
+}
