63af15299806885c1c5fc64bd1e47e74546ceb50
santosh <sreddy08840@gmail.com>
2026-03-14 22:16:25 +0530

    substring1

---
+++ String/substring1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class substring1 {
+	
+	static void findSizeSubString(String s1,int size){
+		
+		for(int i=0;i<=s1.length()-size;i++) {
+			String t ="";
+			for(int j=i;j<size+i;j++) {
+				t=t+s1.charAt(j);
+				
+			}
+			System.out.println(t);
+		}
+		
+		
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		String s1 = scan.next();
+		int size =scan.nextInt();
+		
+findSizeSubString(s1,size);
+		
+	}
+}
