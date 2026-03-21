c522d1724f22a18708ae934492767653aabc07a0
Santosh <sreddy08840@gmail.com>
2026-03-21 18:04:17 +0530

    Remove unnecessary blank lines in Demo.java

--- multithreding/Demo.java
+++ multithreding/Demo.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 class Demo1 extends Thread{
 	@Override
 	public void run() {
@@ -41,7 +40,6 @@ class Demo3 extends Thread{
 	
 	}
 }
-
 public class Demo {
 	public static void main(String[] args) {
 		Demo1 d1=new Demo1();
