55c82800244769db16251abc6024fd2b8804b831
Santosh <sreddy08840@gmail.com>
2026-03-23 12:17:23 +0530

    Add array element deletion functionality
    Implement deletion of an element from an array.

--- array/ArrayDeletion.java
+++ array/ArrayDeletion.java
@@ -2,11 +2,9 @@ public class ArrayDeletion {
     public static void main(String[] args) {
         int[] arr = {10, 20, 30, 40, 50};
         int n = 5; // current size
-
         System.out.print("Before Deletion: ");
         for (int i = 0; i < n; i++) System.out.print(arr[i] + " ");
         System.out.println();
-
         int pos = 2; // delete element at index 2 (30)
         int deleted = arr[pos];
         for (int j = pos; j < n - 1; j++) {
