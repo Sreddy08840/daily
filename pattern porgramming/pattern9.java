7381cf706704b91aaa7a743dd129a81cdb4a9ff5
santosh <sreddy08840@gmail.com>
2026-04-01 16:55:14 +0530

    pattern9

---
+++ pattern porgramming/pattern9.java
@@ -0,0 +1,14 @@
+public class pattern9 {
+	public static void main(String[] args) {
+		for(int i = 1;i<= 5;i++) {
+			for (int k = 1;k<=i;k++) {
+				System.out.print(" ");
+			}
+			for(int j = 1;j<=5-i+1;j++) {
+				System.out.print("*");		
+				}
+			System.out.println();
+		}
+		
+	}
+}
