cc22b086a54637aa9dfc5834f2ffb95c32eefe81
santosh <sreddy08840@gmail.com>
2026-04-05 17:58:32 +0530

    pattern14

---
+++ pattern porgramming/pattern14.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class pattern14 {
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);	
+	int n = sc.nextInt();
+	
+	
+	
+	for(int i = 1;i<=n;i++) {
+		for(int j =i;j<=n;j++) {
+			System.out.print(" ");
+		}
+		for (int k =1;k<=(2*i-1);k++) {
+			if(k==1||i==n||k==(2*i-1)) {
+			System.out.print("*");
+			}else {
+				System.out.print(" ");
+			}
+		}
+		System.out.println();
+	}
+	}
+}
