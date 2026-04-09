d4db999cf98cabed6d68fef225c6b5720414b88e
santosh <sreddy08840@gmail.com>
2026-04-09 20:00:47 +0530

    static learnning

---
+++ pattern porgramming/Program1.java
@@ -0,0 +1,60 @@
+
+class Program1
+
+{
+
+    static int a;
+
+    
+
+    static
+
+    {
+
+       System.out.println("Inside static-block");
+
+        a = 10;
+
+        b = 200;
+
+    }
+
+
+
+    int b;
+
+
+
+    void disp()
+
+    {
+
+    	System.out.println("inside nin-static method");
+
+    	System.out.println(a);
+
+    	System.out.println(b);
+
+    }
+
+    
+
+    public static void main(String[] args)
+
+    {
+
+        Program1 p1 = new Program1();
+
+        p1.a = 111;
+
+        p1.disp();
+
+        Program1 p2 = new Program1();
+
+        p2.a = 222;
+
+        p2.disp();
+
+    }
+
+}
