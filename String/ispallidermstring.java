1cecb6dfcdb673316d2117afb77d2eccccf36286
santosh <sreddy08840@gmail.com>
2026-04-22 21:14:04 +0530

    removing space

--- String/ispallidermstring.java
+++ String/ispallidermstring.java
@@ -33,12 +33,6 @@ public class ispallidermstring {
 		return true;
 	}
 	
-	
-	
-	
-	
-	
-	
 	public static void main(String[] args) {
 		Scanner scan=new Scanner(System.in);
 		String s=scan.next();
